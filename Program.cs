c2d5b9de1d997e91907145f813460c1f3ed63b61
Alonso Gutierrez <alonso.gutierrezm@outlook.com>
2023-02-21 08:36:13 -0600

    Primer commit del proyecto de ArraysMerge

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] a = {1,2,3};
+int[] b = {3,4,5};
+int[] c = new int[a.Length + b.Length];
+
+for(int x = 0; x < b.Length; x++)
+{
+    c[x] = b[x];
+}
+
+int y;
+y = 0;
+for(int x = b.Length; x < c.Length; x++)
+{
+    c[x] = a[y];
+    y++;
+}
+
+Array.Sort(c);
+
+foreach(var val in c)
+{
+    Console.WriteLine($"Valor {val}");
+}
